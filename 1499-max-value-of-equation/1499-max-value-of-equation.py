16e67a3282c83f53eac9f92fd73e08eac16232f3
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2022-05-09 16:31:02 +0530

    Time: 2016 ms (21.94%), Space: 60.4 MB (77.12%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -1,22 +1,12 @@
 class Solution:
     def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
-        deque = collections.deque()
-        res = -math.inf
-
-        for point in points:
-            x, y = point[0], point[1]
-            
-            while deque and x - deque[0][0] > k:
-                deque.popleft()
-            
-            if deque:
-                res = max(res, (y + deque[0][1]) + (x - deque[0][0]))
-            
-            # while deque AND any equation including (x, y) will always yield a greater
-			# (or ==) result than any equation including (deque[-1][0], deque[-1][1])
-            while deque and (deque[-1][0] - x) + (y - deque[-1][1]) >= 0:
-                deque.pop()
-            
-            deque.append(point)
-        
-        return res
+        ans = float('-inf')
+        h = []  # max heap (-(points[i][1]-points[i][0]), points[i][0])
+        ans = float('-inf')
+        for xj, yj in points:
+            while h and abs(h[0][1]-xj)>k:
+                heappop(h)
+            if h:
+                ans = max(ans, -h[0][0]+xj+yj)
+            heappush(h, (-(yj-xj), xj))
+        return ans
