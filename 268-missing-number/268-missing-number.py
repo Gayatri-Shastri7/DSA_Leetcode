6737dd363074df35875c4691325e7d81f7b558c8
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2022-09-05 08:44:04 +0530

    Time: 232 ms (46.79%), Space: 15.1 MB (98.53%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,26 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+'''
+        
         nums.sort()
         for i in range(len(nums)):
             if i != nums[i]:
                 return i
         return len(nums)
+'''
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        i=0
+        while i < n: 
+            if nums[i]<n and nums[i]!=i: 
+                s = nums[i]
+                nums[i], nums[s] = nums[s], nums[i]
+            else:
+                i += 1 
+        for i in range(n):
+            if nums[i]!=i: 
+                return i
+        return n
 
             
         
