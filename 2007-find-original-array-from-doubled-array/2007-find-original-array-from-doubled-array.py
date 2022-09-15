171384e929c45eb7b792b098b561d1435a35fcd2
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2022-09-15 22:56:43 +0530

    Time: 3499 ms (10.26%), Space: 29.1 MB (97.66%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,16 @@
+'''
+
+Find Original Array From Doubled Array
+
+'''
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        hq = []
+        ans = []
+        for i in range(len(changed)):
+            if hq and hq[0] * 2 == changed[i]:
+                ans.append(heapq.heappop(hq))
+            else:
+                heapq.heappush(hq, changed[i]) 
+        return ans if not hq else []
